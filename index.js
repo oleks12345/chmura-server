0e7b54e4332769ded2730c4da453345c203badbc
uniatowi <aleksander.uniatowicz@nokia.com>
2022-02-01 13:46:05 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('*', (req, res) => {
-  res.send('Goodbye mars!');
+app.get("*", (req, res) => {
+  res.send("Goodbye mars!!!");
 });
 
 app.listen(PORT, HOST);
